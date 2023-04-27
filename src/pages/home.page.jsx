d6409c81545929f6d32f18d7ca82019d78b34363
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-27 12:08:46 +0700

    import navbar di homepage

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavbarComponent from '../components/NavbarComponent/navbar.component';
 
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-center'>
+        <NavbarComponent />
         Ini Halaman Home
       </h1>
     </div>
