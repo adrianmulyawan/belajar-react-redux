bb0568012e48881879628eddd24d2d0820ac6b73
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-27 13:42:18 +0700

    Buat reducers untuk contact (segala yang berhubungan dengan contact)

---
+++ src/reducers/contact/contact.js
@@ -0,0 +1,12 @@
+// Digunakan untuk menyimpan yang berhubungan dengan contact
+
+const initialState = {};
+
+const contact = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  };
+};
+
+export default contact;
