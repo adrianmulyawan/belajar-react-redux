4df2cd96fe24b061114fcdf9a6d81e6170abcd4e
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-27 11:54:31 +0700

    tambah halaman homepage (halaman utama)

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-center'>
+        Ini Halaman Home
+      </h1>
+    </div>
+  );
+}
+
+export default HomePage;
