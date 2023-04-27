8512ac0df0c2343008e7cd940199a0c142dac904
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-27 13:43:42 +0700

    config react reduxnya

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// > Bootstrap 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
+
+// > React Redux
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+// > Import Reducers
+import reducers from './reducers';
+
+// > buat store reducers
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
