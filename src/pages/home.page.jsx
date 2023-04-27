28e7bd92de59c642eab795b9c6456ba2d84ae991
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-27 12:08:59 +0700

    edit posisi navbar

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -3,12 +3,14 @@ import NavbarComponent from '../components/NavbarComponent/navbar.component';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className='text-center'>
-        <NavbarComponent />
-        Ini Halaman Home
-      </h1>
-    </div>
+    <>
+      <NavbarComponent />
+      <div className="container mt-3">
+        <h1 className='text-center'>
+          Ini Halaman Home
+        </h1>
+      </div>
+    </>
   );
 }
 
