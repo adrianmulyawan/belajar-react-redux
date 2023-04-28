e67380ca8bd1bf03b1ca17e035704376eb8fd640
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-28 17:36:55 +0700

    Tambah reducer untuk handle state tambah kontak

--- src/reducers/contact/contact.reducer.js
+++ src/reducers/contact/contact.reducer.js
@@ -1,10 +1,15 @@
 // Menyimpan store, atau state yang berhubungan dengan data contact
-import { GET_LIST_CONTACT } from "../../actions/contact.action";
+import { GET_LIST_CONTACT, ADD_NEW_CONTACT } from "../../actions/contact.action";
 
 const initialState = {
+  // > SHOW ALL CONTACT
   getListContactResult: false,
   getListContactLoading: false,
   getListContactError: false,
+  // > ADD NEW CONTACT
+  addContactResult: false,
+  addContactLoading: false,
+  addContactError: false,
 };
 
 const contactReducer = (state = initialState, action) => {
@@ -14,12 +19,21 @@ const contactReducer = (state = initialState, action) => {
     // => Maka case ini tidak akan dibaca
     case GET_LIST_CONTACT:
       console.info(action.payload, '4. Masuk ke Reducer!');
-      
+
       return {
         ...state,
         getListContactResult: action.payload.data,
         getListContactLoading: action.payload.loading,
         getListContactError: action.payload.errorMessage
+      }
+    case ADD_NEW_CONTACT:
+      console.info(action.payload, '4. Masuk ke Reducer!');
+
+      return {
+        ...state,
+        addContactResult: action.payload.data,
+        addContactLoading: action.payload.loading,
+        addContactError: action.payload.errorMessage
       }
     default:
       return state;
