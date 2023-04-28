250083f1b89886221c9812cdb92794aa2192b462
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-28 10:53:31 +0700

    Tambah route baru untuk halaman contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home.page';
+import ContactPage from './pages/Contact/contact.page';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={ <HomePage /> } />
+          <Route path='/contact' element={ <ContactPage /> } />
         </Routes>
       </BrowserRouter>
     </>
