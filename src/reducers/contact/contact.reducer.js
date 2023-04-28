9255b013ab781a5d0b3e7378a586ef802d783810
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-28 09:39:15 +0700

    Setup reducer untuk kontak dan import di file index.js

---
+++ src/reducers/contact/contact.reducer.js
@@ -0,0 +1,12 @@
+// Menyimpan store, atau state yang berhubungan dengan data contact
+
+const initialState = {};
+
+const contactReducer = (state = initialState, action) => {
+  switch (action.type) {  
+    default:
+      return state
+  };
+};
+
+export default contactReducer;
